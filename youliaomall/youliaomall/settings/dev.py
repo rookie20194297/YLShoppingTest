26adc55082e50477342fbe04f03147dd1b5443fa
rookie20194297 <1205399630@qq.com>
2021-08-18 21:53:44 +0800

    master

--- youliaomall/youliaomall/settings/dev.py
+++ youliaomall/youliaomall/settings/dev.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(BASE_DIR)
+
+#追加导包路径
+sys.path.insert(0, os.path.join(BASE_DIR,'apps'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'#用户模块
 ]
 
 MIDDLEWARE = [
