4c98bec56b1478132e4846cd8eb29c3f9af0c6fc
rookie20194297 <1205399630@qq.com>
2021-08-21 11:45:31 +0800

    master

--- youliaomall/youliaomall/apps/carts/views.py
+++ youliaomall/youliaomall/apps/carts/views.py
@@ -1,4 +1,3 @@
-from django.http import response
 from django.shortcuts import render
 from django.views import View
 import json
@@ -73,12 +72,14 @@ class CartsView(View):
                 'count':count,
                 'selected':selected
             }
-            #购物数据重写回去cooki
+            #购物数据重写回cookie
             cart_dict_bytes=pickle.dumps(cart_dict)
             cart_str_bytes=base64.b64encode(cart_dict_bytes)
-            cart_str=cart_str_bytes.decode()
+            cookie_cart_str=cart_str_bytes.decode()
             response=http.JsonResponse({"code":RETCODE.OK,"errmsg":"OK"})
-            response.set_cookie('carts','cookie_cart_str')
+            response.set_cookie('carts',cookie_cart_str)
+            # 响应结果
+            return response
     
     def get(self,request):
         """查询购物车"""
@@ -188,11 +189,8 @@ class CartsView(View):
             # 获取cookie中的购物车数据，并且判断是否有购物车数据
             cart_str = request.COOKIES.get('carts')
             if cart_str:
-                # 将 cart_str转成bytes类型的字符串
                 cart_str_bytes = cart_str.encode()
-                # 将cart_str_bytes转成bytes类型的字典
                 cart_dict_bytes = base64.b64decode(cart_str_bytes)
-                # 将cart_dict_bytes转成真正的字典
                 cart_dict = pickle.loads(cart_dict_bytes)
             else:
                 cart_dict = {}
@@ -214,11 +212,8 @@ class CartsView(View):
                 'default_image_url': sku.default_image.url
             }
 
-            # 将cart_dict转成bytes类型的字典
             cart_dict_bytes = pickle.dumps(cart_dict)
-            # 将cart_dict_bytes转成bytes类型的字符串
             cart_str_bytes = base64.b64encode(cart_dict_bytes)
-            # 将cart_str_bytes转成字符串
             cookie_cart_str = cart_str_bytes.decode()
 
             # 将新的购物车数据写入到cookie
@@ -227,4 +222,108 @@ class CartsView(View):
 
             # 响应结果
             return response
-        
+    
+    def delete(self,request):
+        """购物车删除功能"""
+        json_dict = json.loads(request.body.decode())
+        sku_id = json_dict.get('sku_id')
+
+        # 判断sku_id是否存在
+        try:
+            SKU.objects.get(id=sku_id)
+        except SKU.DoesNotExist:
+            return http.HttpResponseForbidden('商品不存在')
+
+        # 判断用户是否登录
+        user = request.user
+        if user is not None and user.is_authenticated:
+            # 用户已登录，修改redis购物车
+            redis_conn = get_redis_connection('carts')
+            pl = redis_conn.pipeline()
+            pl.hdel('carts_%s'%user.id,sku_id)#删除
+            #注意同时也要删除勾选状态
+            pl.serm('selected_%s'%user.id,sku_id)
+            pl.execute()
+
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK'})
+        else:
+            # 用户未登录，修改cookie购物车
+            # 获取cookie中的购物车数据，并且判断是否有购物车数据
+            cart_str = request.COOKIES.get('carts')
+            if cart_str:
+                cart_str_bytes = cart_str.encode()
+                cart_dict_bytes = base64.b64decode(cart_str_bytes)
+                cart_dict = pickle.loads(cart_dict_bytes)
+            else:
+                cart_dict = {}
+            
+            # 将新的购物车数据写入到cookie
+            response = http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK'})
+            
+            #删除字典中的指定key
+            if sku_id in cart_dict:
+                del cart_dict[sku_id]
+                cart_dict_bytes = pickle.dumps(cart_dict)
+                cart_str_bytes = base64.b64encode(cart_dict_bytes)
+                cookie_cart_str = cart_str_bytes.decode()
+                response.set_cookie('carts', cookie_cart_str)
+            else:
+                pass
+           
+            # 响应结果
+            return response
+
+class CartsSelectAllView(View):
+    """全选购物车"""
+    def put(slef,request):
+        json_dict = json.loads(request.body.decode())
+        selected = json_dict.get('selected',True)
+
+        if selected:
+            if not isinstance(selected,bool):
+                return http.HttpResponseForbidden('参数selected 错误')
+
+        # 判断用户是否登录
+        user = request.user
+        if user.is_authenticated:
+            redis_conn=get_redis_connection('carts')
+            redis_carts=redis_conn.hgetall('carts_%s'% user.id)
+            redis_sku_ids=redis_carts.keys()
+            if selected:
+                #全选
+                redis_conn.sadd('selected_%s'% user.id,*redis_sku_ids)
+            else:
+                redis_conn.sadd('selected_%s'% user.id,*redis_sku_ids)
+            #响应
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'OK'})
+        else:
+            # 用户未登录，修改cookie购物车
+            # 获取cookie中的购物车数据，并且判断是否有购物车数据
+            cart_str = request.COOKIES.get('carts')
+            response=http.JsonResponse({"code":RETCODE.OK,"errmsg":"OK"})
+            if cart_str:
+                cart_str_bytes = cart_str.encode()
+                cart_dict_bytes = base64.b64decode(cart_str_bytes)
+                cart_dict = pickle.loads(cart_dict_bytes)
+                for sku_id in cart_dict:
+                    cart_dict[sku_id]['selected']=selected
+                #购物数据重写回cookie
+                cart_dict_bytes=pickle.dumps(cart_dict)
+                cart_str_bytes=base64.b64encode(cart_dict_bytes)
+                cookie_cart_str=cart_str_bytes.decode()
+                response.set_cookie('carts',cookie_cart_str)
+            # else:
+            #     cart_dict = {}
+            
+            # for sku_id in cart_dict:
+            #     cart_dict[sku_id]['selected']=selected
+            
+            # #购物数据重写回cookie
+            # cart_dict_bytes=pickle.dumps(cart_dict)
+            # cart_str_bytes=base64.b64encode(cart_dict_bytes)
+            # cookie_cart_str=cart_str_bytes.decode()
+            # response=http.JsonResponse({"code":RETCODE.OK,"errmsg":"OK"})
+            # response.set_cookie('carts',cookie_cart_str)
+            
+            # 响应结果
+            return response
