cc75266971f41bc075d04acea046055aeee9fd23
rookie20194297 <1205399630@qq.com>
2021-08-21 21:07:09 +0800

    master

--- youliaomall/youliaomall/apps/contents/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-
-urlpatterns = [
-    # 首页广告: '/'
-    url(r'^$', views.IndexView.as_view(), name='index'),
-]
--- youliaomall/youliaomall/apps/users/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from . import views
-urlpatterns = [
-    #用户注册
-    url(r'^register/$',views.RegisterView.as_view()),
-]
