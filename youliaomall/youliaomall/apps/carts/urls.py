37451469e356195745c7400ae84f60b572e4aaf6
rookie20194297 <1205399630@qq.com>
2021-08-21 11:44:37 +0800

    master

--- youliaomall/youliaomall/apps/carts/urls.py
+++ youliaomall/youliaomall/apps/carts/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.views.generic.base import View
 
 from . import views
 
 urlpatterns = [
     #购物车管理
     url(r'^carts/$',views.CartsView.as_view(),name='info'),
+    #q全选的url
+    url(r'carts/selection/',views.CartsSelectAllView.as_view()),
 ]
